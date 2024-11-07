7e89e69837e6bc48416579ab99de4e145e6479f8
jegan31 <109581881+jegan31@users.noreply.github.com>
2024-11-07 22:15:27 +0530

    Update code.cpp

--- 1.Two sum/code.cpp
+++ 1.Two sum/code.cpp
@@ -1,1 +1,18 @@
-
+//Here i have solve  the problem in O(n) time complexity with single for loop
+//here we need to return the position of target summ array elements so first  we take the first element from the array and 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> mpp;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            int val=nums[i];
+            int diff=target-val;
+            if(mpp.find(diff)!=mpp.end()){
+                return {mpp[diff],i};
+            }
+            mpp[val]=i;
+        }
+        return {-1,-1};
+    }
+};
