643ee93b543d65a93dc2e46831e131591d5d5cd9
jegan31 <109581881+jegan31@users.noreply.github.com>
2024-11-08 19:23:18 +0530

    Update code.cpp

--- 1480. Running Sum of 1d Array/code.cpp
+++ 1480. Running Sum of 1d Array/code.cpp
@@ -1,1 +1,12 @@
-
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int n = nums.size(),cs =0;
+        vector<int> ans(n);
+        for(int i = 0;i<n;i++){
+            cs+=nums[i];
+            ans[i] = cs;
+        }
+        return ans;
+    }
+};
