9cb8fa8f14b2de4ffdcde8b4adfa060f0b2d6fb0
jegan31 <109581881+jegan31@users.noreply.github.com>
2024-11-08 19:40:22 +0530

    Update code.cpp

--- 1672. Richest Customer Wealth/code.cpp
+++ 1672. Richest Customer Wealth/code.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int maxsum=0,sum;
+        for(int i=0;i<accounts.size();i++){
+            sum=0;
+            for(int j=0;j<accounts[i].size();j++){
+                sum+=accounts[i][j];
+            }
+            if(sum>maxsum)
+                maxsum=sum;
+        }
+        return maxsum;
+    }
+};
