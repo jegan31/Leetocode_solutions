553d85d31450c3154ed7fb8793e841ca35911af7
jegan31 <109581881+jegan31@users.noreply.github.com>
2024-11-10 12:48:23 +0530

    Update code.cpp

--- 1957. Delete Characters to Make Fancy String/code.cpp
+++ 1957. Delete Characters to Make Fancy String/code.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    string makeFancyString(string s) {
+        string ans="";
+        int n = s.size(),j,cnt;
+        if(n<=2)
+            return s;
+        for(int i=0;i<n-2;i++){
+            if(s[i]==s[i+1] && s[i]==s[i+2])
+                continue;
+            ans+=s[i];
+        }
+        ans+=s[n-2];
+        ans+=s[n-1];
+        return ans;
+    }
+};
