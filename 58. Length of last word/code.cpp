782eafd22554b8f5d09e35a26f335d4eb41f7b12
jegan31 <109581881+jegan31@users.noreply.github.com>
2024-11-17 18:59:36 +0530

    Update code.cpp

--- 58. Length of last word/code.cpp
+++ 58. Length of last word/code.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n=s.size(),cnt=0;
+        int i = n - 1;
+        while (i >= 0 && s[i] == ' ') {
+            i--;
+        }
+    
+        // Count the length of the last word
+        while (i >= 0 && s[i] != ' ') {
+            cnt++;
+            i--;
+        }
+        return cnt;
+    }
+};
