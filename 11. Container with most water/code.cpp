ba5503939c015d0b086323c13f6b37988e890178
jegan31 <109581881+jegan31@users.noreply.github.com>
2024-11-19 13:53:12 +0530

    Update code.cpp

--- 11. Container with most water/code.cpp
+++ 11. Container with most water/code.cpp
@@ -1,1 +1,25 @@
-
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int area=0,maxarea=-1;
+        // for(int i=0;i<n;i++){
+        //     area=0;
+        //     for(int j=i;j<n;j++){
+        //         area = min(height[i],height[j]) * (j-i);
+        //         if(area>maxarea)
+        //             maxarea=area;
+        //     }
+        // }
+        int left = 0,right = height.size()-1;
+        while(left<right){
+            area = min(height[left],height[right]) * (right-left);
+            if(area>maxarea)
+                     maxarea=area;
+            if(height[left]<height[right])
+                left++;
+            else
+                right--;
+        }
+        return maxarea;
+    }
+};
